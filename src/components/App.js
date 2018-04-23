109e5168131c4268f824577df663786df7ff06fd
hueter <mthueter@gmail.com>
2018-04-22 17:55:13 -0700

    first pass at annotating everything

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import ColorBoxContainer from '../containers/ColorBoxContainer';
 
 class App extends Component {
+  /**
+   * Note that App specifically renders the Container component,
+   *  which wraps the display component ColorBox. Any time a component
+   *  subscribes to Redux, its parent needs to render its container instead of
+   *  the regular component.
+   */
   render() {
     return (
       <div className="App">
--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -4,17 +4,35 @@ import './ColorBox.css';
 import { logStuff } from '../helpers';
 
 class ColorBox extends Component {
+  /**
+   * PLEASE IGNORE
+   * This method is only implemented to logStuff; there is no
+   *  component functionality
+   */
   componentDidMount() {
-    logStuff('Component Mounted', '#EF6C00', ['props', this.props]);
+    logStuff('Component Mounted', '#1565C0', ['props', this.props]);
   }
+
+  /**
+   * PLEASE IGNORE
+   *  This method is only implemented to logStuff; there is no
+   *  component functionality
+   */
   componentDidUpdate(prevProps) {
-    logStuff('Component Updated', '#EF6C00', [
+    logStuff('Component Updated', '#1565C0', [
       'Previous Props',
       prevProps,
       'Current Props',
       this.props
     ]);
   }
+
+  /**
+   * This is a "dumb" component in that it has no state and only renders
+   *  what it gets from Redux (via the container). The component also
+   *  has no idea what "onClick" does, other than that it's a function.
+   *  The ColorBoxContainer is the smart component.
+   */
   render() {
     return (
       <div>
@@ -32,14 +50,31 @@ class ColorBox extends Component {
   }
 }
 
+/**
+ * The component should never render with these defaults.
+ *  These are here almost as a smoke test to make sure that
+ *  the container is being rendered instead of this one.
+ *  The container will pass the Redux default props, where the color
+ *  is "gray" (check in src/store/reducers).
+ */
 ColorBox.defaultProps = {
   changeColor: () => alert('Color Button clicked but nothing happened'),
   randomColor: 'black'
 };
 
+/**
+ *  Prop Types in this case are mostly for the purpose of
+ *  self-documenting what the props are.
+ */
 ColorBox.propTypes = {
   changeColor: PropTypes.func.isRequired,
   randomColor: PropTypes.string.isRequired
 };
 
+/**
+ * 'export default' is the ES6 Module analog to the
+ *  Common.JS equivalent 'module.exports = ColorBox'
+ *  When importing a default export component, no destructuring is involved,
+ *  e.g. 'import ColorBox from './ColorBox';
+ */
 export default ColorBox;
